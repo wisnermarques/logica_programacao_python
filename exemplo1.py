e4c57f6c870cf3a6354ff044c4d917b86e11bb59
wisner <wisner.marquesexcel@gmail.com>
2024-06-07 21:22:43 -0300

    criação de arquivos de exemplo

---
+++ exemplo1.py
@@ -0,0 +1,15 @@
+print('Meu primeiro programa!')
+
+print(5+2)
+
+print(6-4)
+
+print(5*4)
+
+print(10/4)
+
+print(10//4)
+
+print(5%2)
+
+print(2**3)
---
+++ exemplo2.py
@@ -0,0 +1,12 @@
+'''
+data: 07/06/2024
+Desenvolvido por: Wisner
+'''
+
+# Variáveis
+
+nota1 = float(input('digite a nota 1: '))
+nota2 = float(input('digite a nota 2: '))
+media = (nota1 + nota2) / 2
+
+print('Média: ', media)
